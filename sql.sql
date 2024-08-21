d24bdab1f567a00edd6f9edcf4d40337fb40d47c
Hasib <130287429+Haaasib@users.noreply.github.com>
2024-08-22 02:27:41 +0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS `jomidar_fentanyl` (
     `model` varchar(255) NOT NULL,
     `position` text NOT NULL,
     `rotation` text NOT NULL,
-    `amount` int(11) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
